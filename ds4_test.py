be6aa54162c772b0042b802a9f43295f2a9830dd
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 07:45:05 +0900

    Update ds4_test.py

--- ds4_test.py
+++ ds4_test.py
@@ -4,16 +4,9 @@ from time import sleep
 from pyPS4Controller.controller import Controller
 
 
-# Translate controller input into motor output values
 
 def transf(raw):
     temp = raw/65534 * 5
-    # Filter values that are too weak for the motors to move
-    return round(temp, 1)
-
-def transf1(raw):
-    temp = raw/65534 * 5
-    # Filter values that are too weak for the motors to move
     return round(temp, 1)
 
 class MyController(Controller):
@@ -21,33 +14,13 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
     
-    def on_L3_down(self, value):
-        value = transf(value)
-        if value == 0:
-            print(value)
-        else:
-            print(value)
-            
-    def on_L3_up(self, value):
-        value = transf1(value)
-        if value == 0:
-            print(value)
-        else:
-            print(value)
-
     def on_R3_down(self, value):
         value = transf(value)
-        if value == 0:
-            print(value)
-        else:
-            print(value)
+        print(value)
             
     def on_R3_up(self, value):
         value = transf1(value)
-        if value == 0:
-            print(value)
-        else:
-            print(value)
+        print(value)
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen()
