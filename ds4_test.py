707b71220b034f4ee5bfe804668ce1812627f8a7
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 07:40:09 +0900

    Update ds4_test.py

--- ds4_test.py
+++ ds4_test.py
@@ -9,20 +9,12 @@ from pyPS4Controller.controller import Controller
 def transf(raw):
     temp = (raw+32767)/65534
     # Filter values that are too weak for the motors to move
-    if abs(temp) < 0.9:
-        return 0
-    # Return a value between 0.3 and 1.0
-    else:
-        return round(temp, 1)
+    return round(temp, 1)
 
 def transf1(raw):
     temp = (abs(raw)+32767)/65534
     # Filter values that are too weak for the motors to move
-    if abs(temp) < 0.9:
-        return 0
-    # Return a value between 0.3 and 1.0
-    else:
-        return round(temp, 1)
+    return round(temp, 1)
 
 class MyController(Controller):
 
