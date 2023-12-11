c097f1f2893b406a5fff84086c088aef4998d1ce
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 07:38:02 +0900

    Update ds4_test.py

--- ds4_test.py
+++ ds4_test.py
@@ -1,8 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from time import sleep
 from pyPS4Controller.controller import Controller
 
+
+# Translate controller input into motor output values
+
+def transf(raw):
+    temp = (raw+32767)/65534
+    # Filter values that are too weak for the motors to move
+    if abs(temp) < 0.9:
+        return 0
+    # Return a value between 0.3 and 1.0
+    else:
+        return round(temp, 1)
+
+def transf1(raw):
+    temp = (abs(raw)+32767)/65534
+    # Filter values that are too weak for the motors to move
+    if abs(temp) < 0.9:
+        return 0
+    # Return a value between 0.3 and 1.0
+    else:
+        return round(temp, 1)
+
 class MyController(Controller):
+
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
+    
+    def on_L3_down(self, value):
+        value = transf(value)
+        if value == 0:
+            print(value)
+        else:
+            print(value)
+            
+    def on_L3_up(self, value):
+        value = transf1(value)
+        if value == 0:
+            print(value)
+        else:
+            print(value)
+
+    def on_R3_down(self, value):
+        value = transf(value)
+        if value == 0:
+            print(value)
+        else:
+            print(value)
+            
+    def on_R3_up(self, value):
+        value = transf1(value)
+        if value == 0:
+            print(value)
+        else:
+            print(value)
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen()
