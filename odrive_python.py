cf4ef4d2d788129a76b3a59e954a67468cf200c8
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-11 23:40:04 +0900

    Update odrive_python.py

--- odrive_python.py
+++ odrive_python.py
@@ -6,3 +6,6 @@ print(str(odrv0.vbus_voltage))
 
 odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 odrv0.axis0.controller.input_vel = 2
+
+time.sleep(5)
+odrv0.axis0.requested_state = AXIS_STATE_IDLE
