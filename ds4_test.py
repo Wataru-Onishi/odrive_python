77f223c5d0bbe0c666f47707b295b4425fe4d032
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 01:29:08 +0900

    Update ds4_test.py

--- ds4_test.py
+++ ds4_test.py
@@ -5,17 +5,17 @@ from pyPS4Controller.controller import Controller
 
 def transf(raw):
     temp = (raw+32767)/65534
+    print(temp)
 
+
+# class MyController(Controller):
+
+#     def __init__(self, **kwargs):
+#         Controller.__init__(self, **kwargs)
     
-
-class MyController(Controller):
-
-    def __init__(self, **kwargs):
-        Controller.__init__(self, **kwargs)
-    
-    def on_L3_down(self, value):
-        value = transf(value)
-        print(value)
+#     def on_L3_down(self, value):
+#         value = transf(value)
+#         print(value)
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen()
