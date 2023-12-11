b7ff317e04212001c103583e0c136070fa13d4c5
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 07:51:18 +0900

    Update odrive_python.py

--- odrive_python.py
+++ odrive_python.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from time import sleep
+from pyPS4Controller.controller import Controller
+
 import odrive
 from odrive.enums import *
 
-import time
 
 odrv0 = odrive.find_any()
 print(str(odrv0.vbus_voltage))
@@ -11,4 +15,32 @@ odrv0.axis0.controller.input_vel = 2
 
 time.sleep(5)
 odrv0.axis0.controller.input_vel = 0
+odrv0.axis0.requested_state = AXIS_STATE_IDLE
+
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+
+
+while(True)
+    def transf(raw):
+        temp = raw/65534 * 5
+        return round(temp, 1)
+
+    class MyController(Controller):
+
+        def __init__(self, **kwargs):
+            Controller.__init__(self, **kwargs)
+        
+        def on_R3_down(self, value):
+            value = transf(value)
+            odrv0.axis0.controller.input_vel = value
+            print(value)
+                
+        def on_R3_up(self, value):
+            value = transf(value)
+            odrv0.axis0.controller.input_vel = value
+            print(value)
+
+    
+
+
 odrv0.axis0.requested_state = AXIS_STATE_IDLE
