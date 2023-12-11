5a3c1f77159626141eda434da438d826630c752b
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 01:34:21 +0900

    Update ds4_test.py

--- ds4_test.py
+++ ds4_test.py
@@ -5,17 +5,56 @@ from pyPS4Controller.controller import Controller
 
 def transf(raw):
     temp = (raw+32767)/65534
-    print(temp)
+    # Filter values that are too weak for the motors to move
+    if abs(temp) < 0.9:
+        return 0
+    # Return a value between 0.3 and 1.0
+    else:
+        return round(temp, 1)
 
+def transf1(raw):
+    temp = (abs(raw)+32767)/65534
+    # Filter values that are too weak for the motors to move
+    if abs(temp) < 0.9:
+        return 0
+    # Return a value between 0.3 and 1.0
+    else:
+        return round(temp, 1)
 
-# class MyController(Controller):
+class MyController(Controller):
 
-#     def __init__(self, **kwargs):
-#         Controller.__init__(self, **kwargs)
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
     
-#     def on_L3_down(self, value):
-#         value = transf(value)
-#         print(value)
+    def on_L3_down(self, value):
+        value = transf(value)
+        if value == 0:
 
-controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen()
+            print(value)
+        else:
+            print(value)
+            
+    def on_L3_up(self, value):
+        value = transf1(value)
+        if value == 0:
+            print(value)
+        else:
+            print(value)
+
+    def on_R3_down(self, value):
+        value = transf(value)
+        if value == 0:
+            print(value)
+        else:
+            print(value)
+            
+    def on_R3_up(self, value):
+        value = transf1(value)
+        if value == 0:
+            print(value)
+        else:
+            print(value)
+
+    def on_x_press(self):
+
+    def on_x_release(self):
