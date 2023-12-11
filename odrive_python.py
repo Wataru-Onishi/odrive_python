79a51d9ee14e520e601d29d1af19e6e807ff9cf7
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 08:57:52 +0900

    Update odrive_python.py

--- odrive_python.py
+++ odrive_python.py
@@ -23,23 +23,22 @@ class MyController(Controller):
         Controller.__init__(self, **kwargs)
     
     def on_R3_down(self, value):
-        if(abs(value) <1):
+        value = transf(value)
+        if(value <1):
             value = 0
             odrv0.axis0.controller.input_vel = 0
         else:
-            value = transf(value)
-        odrv0.axis0.controller.input_vel = value
-        print(value)
+            odrv0.axis0.controller.input_vel = value
+            print(value)
             
     def on_R3_up(self, value):
-        if(abs(value) <1):
+        value = transf(value)
+        if(value <1):
             value = 0
             odrv0.axis0.controller.input_vel = 0
-
         else:
-            value = transf(value)
             odrv0.axis0.controller.input_vel = value
-        print(value)
+            print(value)
     
     def on_R3_y_at_rest(self):
         odrv0.axis0.controller.input_vel =0
