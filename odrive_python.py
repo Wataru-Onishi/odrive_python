e8e2e1140b190730c5e8f4f7ad8e717c0c07939c
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2023-12-12 08:01:06 +0900

    Update odrive_python.py

--- odrive_python.py
+++ odrive_python.py
@@ -13,25 +13,28 @@ print(str(odrv0.vbus_voltage))
 odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
 
-while(True):
-    def transf(raw):
-        temp = raw/65534 * 5
-        return round(temp, 1)
 
-    class MyController(Controller):
+def transf(raw):
+    temp = raw/65534 * 5
+    return round(temp, 1)
 
-        def __init__(self, **kwargs):
-            Controller.__init__(self, **kwargs)
-        
-        def on_R3_down(self, value):
-            value = transf(value)
-            odrv0.axis0.controller.input_vel = value
-            print(value)
-                
-        def on_R3_up(self, value):
-            value = transf(value)
-            odrv0.axis0.controller.input_vel = value
-            print(value)
-        
-        def on_x_puless(self):
-            kill = 1
+class MyController(Controller):
+
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+    
+    def on_R3_down(self, value):
+        value = transf(value)
+        odrv0.axis0.controller.input_vel = value
+        print(value)
+            
+    def on_R3_up(self, value):
+        value = transf(value)
+        odrv0.axis0.controller.input_vel = value
+        print(value)
+    
+    def on_x_puless(self):
+        kill = 1
+
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen()
